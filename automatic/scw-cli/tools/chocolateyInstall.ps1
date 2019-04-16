710c40743e15f855c42cbf6caf93b8ed037d2a76
Thor Erik Lie <thor@thorerik.com>
2019-04-16 10:37:53 +0200

    Automatic packaging of scw-cli

---
+++ automatic/scw-cli/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url32 = 'https://github.com/scaleway/scaleway-cli/releases/download/v1.19/scw-windows-amd64.exe'
+$checksum32      = '55eb14959aff9374e282b3dbdd0e17fe84a604c91bf9b5b6507cb02ecddc0c25'
+$checksumType32  = 'sha256'
+$url64 = 'https://github.com/scaleway/scaleway-cli/releases/download/v1.19/scw-windows-i386.exe'
+$checksum64    = '883c8a66cd5a35d85fa89b1ff5705a38cbb7ac6cadb592bd404dc7c52c3e54d3'
+$checksumType64= 'sha256'
+
+Get-ChocolateyWebFile -PackageName 'scaleway-cli' -FileFullPath "$toolsDir\scw.exe" -Url $url32 -Url64 $url64 -Checksum $checksum32 -ChecksumType $checksumType32 -Checksum64 $checksum64 -ChecksumType64 $checksumType64
--- automatic/scw-cli/update.ps1
+++ automatic/scw-cli/update.ps1
@@ -1,6 +1,6 @@
 import-module au
 
-$releases = 'https://github.com/htacg/tidy-html5/releases'
+$releases = 'https://github.com/scaleway/scaleway-cli/releases'
 
 function global:au_SearchReplace {
     @{
@@ -14,13 +14,14 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
     #tidy-5.1.25-win64.zip
-    $re  = "tidy-.+-win(32|64).zip"
+    $re  = "scw-windows-(i386|amd64).exe"
     $url = $download_page.links | ? href -match $re | select -First 2 -expand href
 
-    $version = $url[0] -split '-' | select -Last 1 -Skip 1
+    $version_temp = $download_page.links | ? href -match "scw_(\d*\.\d*)_amd64.deb"
+    $version = $version_temp[0] -split '_' | select -Last 1 -Skip 1
     $url32 = 'https://github.com' + $url[0]
     $url64 = 'https://github.com' + $url[1]
 
